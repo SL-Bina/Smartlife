14d736a50d90314caf3f8ae463228e5e137ec40a
Emil1ff <emilhesenov398@gmail.com>
2025-11-11 15:42:16 +0400

    started

--- src/widgets/layout/navbar.jsx
+++ src/widgets/layout/navbar.jsx
@@ -86,7 +86,7 @@ Navbar.defaultProps = {
   brandName: "West",
   action: (
     <a
-      href="https://www.creative-tim.com/product/material-tailwind-dashboard-react"
+      href="https://www.creative-tim.com/product/west"
       target="_blank"
     >
       <Button variant="gradient" size="sm" fullWidth>
